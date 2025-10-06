9b2cd1d9b3e51e29848ce42e1c71ee2bd0b6e6a7
Jan Kasprzak <jan@markasan.pl>
2025-10-06 07:29:28 +0000

    06.10.2025 0-1-2-3 nadrobienie lekcji

--- first-server/index.js
+++ first-server/index.js
@@ -1,12 +1,26 @@
 import { createServer } from 'node:http';
-import { getActiveResourcesInfo } from 'node:process';
+import { readFileSync } from "node:fs";
+import { URL } from "node:url";
+import { handlePath } from "./src/path_handlers.js";
+
+const index_html = readFileSync("static/index.html");
 
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello world');
+    const request_url = new URL(`http://${host}${req.url}`);
+    const path = request_url.pathname;
+    console.log(`Request: ${req.method} ${request_url.pathname}`);
+      
+    handlePath(request_url.pathname, req, res);
+
+  if (!res.writableEnded) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Site not found!\n');
+  }
 });
 
-const port = 65535;
+
+
+const port = 65534;
 const host = "localhost";
 
 server.listen(port, host, () => {
