c00dd0ca17295bc4c4b22c60ed00f04c8071cbda
gojl <jan@markasan.pl>
2025-12-16 01:12:27 +0100

    projekt03 3x3 PLL Database

---
+++ projekt03/Readme.txt
@@ -0,0 +1,24 @@
+3x3 PLL Database instrukcja konfiguracji i użycia
+Na tej aplikacji możesz przeglądać moje algorytmy i czasy na ostatnią warstwę kostki Rubika.
+Możesz również dodawać, zmieniać i usuwać własne algorytmy i czasy poprzez formularz i przyciski Edytuj / Usuń.
+
+1. Sklonuj repozytorium:
+   git clone https://github.com/Gojl/PZAW-Projekty-Jan-Kasprzak-3C
+
+2. Przejdź do folderu projektu:
+   cd projekt03
+
+3. Zainstaluj potrzebne pakiety Node.js:
+   npm install
+
+4. Jeżeli github nie importuje baz danych to stwórz bazy komendami:
+    node myplls.js
+    node yourplls.js
+
+5. Uruchom serwer:
+   node index.js
+
+6. Otwórz przeglądarkę i przejdź na:
+   http://localhost:6767
+
+
---
+++ projekt03/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import { DatabaseSync } from "node:sqlite";
+import path from "path";
+
+const port = 6767;
+
+const app = express(); 
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+app.set("view engine", "ejs");
+app.use(express.static("main"));
+app.use(express.urlencoded());
+
+const db = new DatabaseSync("./plls/myplls.db");
+
+app.get("/myplls", (req, res) => {
+  const allStmt = db.prepare("SELECT * FROM myplls");
+  const allPLL = allStmt.all();
+  res.render("myplls", { pll: allPLL }); 
+});
+
+const yourDb = new DatabaseSync("./plls/yourplls.db");
+
+app.get("/yourplls", (req, res) => {
+  const stmt = yourDb.prepare("SELECT * FROM yourplls");
+  const data = stmt.all();
+  res.render("yourplls", { pll: data });
+});
+
+app.get("/add-change", (req, res) => {
+  const { name } = req.query;
+  let pllData = null;
+
+  if (name) {
+    pllData = yourDb.prepare("SELECT * FROM yourplls WHERE name = ?").get(name);
+  }
+
+  res.render("add_change", { pll: pllData });
+});
+
+app.post("/add-change-yourpll", (req, res) => {
+  const { name, algorithm, best_time } = req.body;
+
+  const stmt = yourDb.prepare("SELECT * FROM yourplls WHERE name = ?");
+  const existing = stmt.get(name);
+
+  if (existing) {
+    const updateStmt = yourDb.prepare(
+      "UPDATE yourplls SET algorithm = ?, best_time = ? WHERE name = ?"
+    );
+    updateStmt.run(algorithm, best_time, name);
+  } else {
+
+    const insertStmt = yourDb.prepare(
+      "INSERT INTO yourplls (name, algorithm, best_time) VALUES (?, ?, ?)"
+    );
+    insertStmt.run(name, algorithm, best_time);
+  }
+  res.redirect("/yourplls");
+});
+
+app.post("/delete-yourpll", (req, res) => {
+  const { name } = req.body;
+
+  const stmt = yourDb.prepare("DELETE FROM yourplls WHERE name = ?");
+  stmt.run(name);
+
+  res.redirect("/yourplls");
+});
+
+
+app.listen(port, () => { 
+  console.log(`Server listening on http://localhost:${port}`);
+});
