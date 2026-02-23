38269126f311115761ff2c816c932234ef776496
gojl <jan@markasan.pl>
2026-02-23 10:33:31 +0100

    projekt04 pliki + TO DO

---
+++ projekt04/Readme.txt
@@ -0,0 +1,30 @@
+TO DO: 
+1. zjednolicenie bazy danych
+2. Dodanie do tej bazy danych użytkowników 
+3. dodanie view dla login/sign up
+4. dodanie handlera dla admina który może widziec wszystkie bazy danych -> jak ni jest zalogowany to wywala na glowną stronę
+
+3x3 PLL Database instrukcja konfiguracji i użycia
+Na tej aplikacji możesz przeglądać moje algorytmy i czasy na ostatnią warstwę kostki Rubika.
+Możesz również dodawać, zmieniać i usuwać własne algorytmy i czasy poprzez formularz i przyciski Edytuj / Usuń.
+
+1. Sklonuj repozytorium:
+   git clone https://github.com/Gojl/PZAW-Projekty-Jan-Kasprzak-3C
+
+2. Przejdź do folderu projektu:
+   cd projekt04
+
+3. Zainstaluj potrzebne pakiety Node.js:
+   npm install
+
+4. Jeżeli github nie importuje baz danych to stwórz bazy komendami:
+    node myplls.js
+    node yourplls.js
+
+5. Uruchom serwer:
+   node index.js
+
+6. Otwórz przeglądarkę i przejdź na:
+   http://localhost:6767
+
+
---
+++ projekt04/plls/myplls.js
@@ -0,0 +1,45 @@
+import { DatabaseSync } from "node:sqlite";
+
+const db = new DatabaseSync("./plls/myplls.db");
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS myplls (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    algorithm TEXT,
+    best_time TEXT
+  );
+`);
+
+const stmt = db.prepare("SELECT COUNT(*) AS count FROM myplls");
+const row = stmt.get();
+if (row.count === 0) {
+  db.exec(`
+    INSERT INTO myplls (name, algorithm, best_time) VALUES
+    ('Aa', 'x R'' U R'' D2 R U'' R'' D2 R2 x''', 0.97),
+    ('Ab', 'x R2 D2 R U R'' D2 R U'' R x''', 1.12),
+    ('E', 'x'' R U'' R'' D R U R'' D'' R U R'' D R U'' R'' D'' x', 1.04),
+    ('F', 'R'' U'' F'' R U R'' U'' R'' F R2 U'' R'' U'' R U R'' U R', 0.88),
+    ('Ga', 'R2 U R'' U R'' U'' R U'' R2 D U'' R'' U R D''', 1.23),
+    ('Gb', 'R'' U'' R U D'' R2 U R'' U R U'' R U'' R2 D', 1.08),
+    ('Gc', 'R2 U'' R U'' R U R'' U R2 D'' U R U'' R'' D', 0.95),
+    ('Gd', 'R U R'' U'' D R2 U'' R U'' R'' U R'' U R2 D''', 1.31),
+    ('H', 'M2 U M2 U2 M2 U M2', 0.74),
+    ('Ja', 'x R2 F R F'' R U2 r'' U r U2 x''', 1.19),
+    ('Jb', 'R U R'' F'' R U R'' U'' R'' F R2 U'' R''', 0.86),
+    ('Na', 'R U R'' U R U R'' F'' R U R'' U'' R'' F R2 U'' R'' U2 R U'' R''', 1.41),
+    ('Nb', 'R'' U R U'' R'' F'' U'' F R U R'' F R'' F'' R U'' R', 1.07),
+    ('Ra', 'y R U'' R'' U'' R U R D R'' U'' R D'' R'' U2 R''', 0.92),
+    ('Rb', 'R2 F R U R U'' R'' F'' R U2 R'' U2 R', 1.34),
+    ('T', 'R U R'' U'' R'' F R2 U'' R'' U'' R U R'' F''', 1.28),
+    ('Ua', 'M2 U M U2 M'' U M2', 0.81),
+    ('Ub', 'M2 U'' M U2 M'' U'' M2', 1.15),
+    ('V', 'R'' U R U'' R'' f'' U'' R U2 R'' U'' R U'' R'' f R', 0.99),
+    ('Y', 'F R U'' R'' U'' R U R'' F'' R U R'' U'' R'' F R F''', 1.36),
+    ('Z', 'M U M2 U M2 U M U2 M2', 1.22);
+`);
+
+  console.log("Dane Your PLL zostały dodane.");
+} else {
+  console.log("Tabela Your PLL już zawiera dane.");
+}
